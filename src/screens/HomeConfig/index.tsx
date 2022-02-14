54157157dc96073ab611c18e52d79c4c242ab425
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-02-14 20:39:15 -0300

    feat(HomeConfig): implementando ações da tela home config

--- src/screens/HomeConfig/index.tsx
+++ src/screens/HomeConfig/index.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Alert} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  changeLevel,
+  changeName,
+  changeWorkoutDays,
+} from '../../reducers/userReducer';
 import {
   Container,
   Label,
@@ -11,18 +18,47 @@ import {
 } from './styles';
 
 export function HomeConfig() {
-  const days = ['S', 'T', 'Q', 'Q', 'S', 'S', 'D'];
+  const days = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
   const levels = ['Iniciante', 'Intermediário', 'Avançado'];
+  //@ts-ignore
+  const {name, workoutDays, level} = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  function toggleWorkoutDay(day: number) {
+    let newWorkoutDays = [...workoutDays];
+    if (!newWorkoutDays.includes(day)) {
+      newWorkoutDays.push(day);
+      dispatch(changeWorkoutDays(newWorkoutDays));
+      return;
+    }
+
+    if (newWorkoutDays.length > 1) {
+      newWorkoutDays = newWorkoutDays.filter(workoutDay => workoutDay !== day);
+      dispatch(changeWorkoutDays(newWorkoutDays));
+    } else {
+      Alert.alert('', 'Opa, você precisa treinar pelo menos 1 dia');
+    }
+  }
+
+  function handleChangeLevel(level: string) {
+    dispatch(changeLevel(level));
+  }
 
   return (
     <Container>
       <Label>Seu nome:</Label>
-      <Input />
+      <Input value={name} onChangeText={value => dispatch(changeName(value))} />
 
       <Label>Dias em que você treina:</Label>
       <ListArea>
         {days.map((day, index) => (
-          <DayItem key={index}>
+          <DayItem
+            key={index}
+            onPress={() => toggleWorkoutDay(index)}
+            underlayColor="transparent"
+            style={
+              workoutDays.includes(index) ? {backgroundColor: '#A5E8BC'} : {}
+            }>
             <DayText>{day}</DayText>
           </DayItem>
         ))}
@@ -30,9 +66,13 @@ export function HomeConfig() {
 
       <Label>Seu nível:</Label>
       <ListArea>
-        {levels.map((level, index) => (
-          <LevelItem key={index}>
-            <LevelText>{level}</LevelText>
+        {levels.map((userlevel, index) => (
+          <LevelItem
+            underlayColor="transparent"
+            key={index}
+            style={userlevel === level ? {backgroundColor: '#A5E8BC'} : {}}
+            onPress={() => handleChangeLevel(userlevel)}>
+            <LevelText>{userlevel}</LevelText>
           </LevelItem>
         ))}
       </ListArea>
