226337528a73f9474fd9a970ef3a5d7d35a1b191
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-02-26 18:48:25 -0300

    fix: alterando treinos

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {LogBox} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {MainStack} from './src/navigators/MainStack';
 import {store, persistor} from './src/store';
 
 const App = () => {
+  LogBox.ignoreLogs(['react-native-gesture-handler']);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
